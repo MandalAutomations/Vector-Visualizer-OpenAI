90022dcb903012327467375333c33e01247879b8
Evan Allen <evanallen13@github.com>
2025-03-31 19:51:26 +0000

    add decision tree

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.tree import export_graphviz
+from io import StringIO  # Import StringIO from io instead of sklearn.externals.six
+from IPython.display import Image  
+import pydotplus
 
 col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
 pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
@@ -13,10 +17,18 @@ X = pima[feature_cols] # Features
 y = pima.label # Target variable
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
 
-print(X_train)
-# clf = DecisionTreeClassifier()
-# clf = clf.fit(X_train,y_train)
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
+
 
 # y_pred = clf.predict(X_test)
 
 # print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('diabetes.png')
+Image(graph.create_png())
